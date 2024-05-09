115f849931c8cdd2851988382dd8ee3b260bfe8f
Giovanni Bombassaro <giovanni.bombassaro@gmail.com>
2024-05-09 14:39:40 -0300

    feat: Implements cookie and JWT handler

--- src/app/api/authenticate/route.ts
+++ src/app/api/authenticate/route.ts
@@ -1,8 +1,11 @@
+import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { NextResponse, NextRequest } from 'next/server'
+import { jwtDecode, JwtPayload } from 'jwt-decode'
 
 type ResponseData = {
-  message: string
+  status: boolean
+  code?: number
+  message?: string
 }
 
 export async function POST(
@@ -10,6 +13,44 @@ export async function POST(
   res: NextApiResponse<ResponseData>,
 ) {
   console.log('FUNCIONOU', JSON.parse(req.body))
-  return NextResponse.json({ status: 'OK' })
+
+  const apiAddress = process.env.NEXT_PUBLIC_API_URL || ''
+  const username = req.body.username || ''
+  const password = req.body.password || ''
+  const body = { login: username, password }
+
+  try {
+    const payload = await axios.post(`${apiAddress}/Login`, body, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+    const jwt = payload.data.token
+    const decodedjwt: JwtPayload = jwtDecode(jwt)
+    const decodedexp = decodedjwt.exp ? decodedjwt.exp : 0
+    const token = decodedjwt.Data.token
+
+    // const now = new Date()
+    // const decodedexp = now.setDate(now.getDate() + 30)
+    // const exp = new Date(decodedexp * 1000).toUTCString()
+
+    if (!token) {
+      res
+        .status(500)
+        .send({ status: false, code: 500, message: 'Token problem' })
+    }
+
+    res.setHeader(
+      'Set-Cookie',
+      `sid=${token}; Expires=${decodedexp}; SameSite=Strict; Path=/;`,
+    )
+    res.status(200).json({ status: true })
+  } catch (e: any) {
+    const apiErrorMessage = e?.response?.data?.error || ''
+    const requestErrorCode = e?.response?.status || ''
+    console.log('[Fetch Error]', e)
+    res
+      .status(500)
+      .send({ status: false, code: requestErrorCode, message: apiErrorMessage })
+  }
+  // return NextResponse.json({ status: 'OK' })
   // res.status(200).json({ message: 'Hello from Next.js!' })
 }
