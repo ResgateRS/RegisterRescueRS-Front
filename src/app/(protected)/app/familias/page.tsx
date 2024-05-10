bc702137dd40b78964f9b446696484bb7ead9e67
Lorenzo <lorenzo.acetii@gmail.com>
2024-05-10 15:32:05 -0300

    chore: create branch

--- src/app/(protected)/app/familias/page.tsx
+++ src/app/(protected)/app/familias/page.tsx
@@ -1,3 +1,28 @@
-export default function Test() {
-  return <div>Teste</div>
+import { listFamilies } from '@/api/list-families'
+import { ErrorContainer } from '../../../(public)/(landing-page)/_components/error-container'
+import Card from './card'
+import HeaderCadastroFamilia from './header'
+import SearchCadastroFamilia from './search'
+
+export default async function CadastroFamiliaPage() {
+  const { data, message, result } = await listFamilies({
+    pageSize: 4,
+  })
+
+  if (result === 1) {
+    return (
+      <>
+        <HeaderCadastroFamilia />
+        <div className="container mx-auto px-4 py-12">
+          <SearchCadastroFamilia />
+
+          {data.map((family) => (
+            <Card key={family.familyId} family={family} />
+          ))}
+        </div>
+      </>
+    )
+  }
+
+  return <ErrorContainer message={message} />
 }
