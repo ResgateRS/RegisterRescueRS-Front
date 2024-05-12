b7868b5d6ffde916a1a2775b5968147149005fd1
Lorenzo <lorenzo.acetii@gmail.com>
2024-05-12 13:26:58 -0300

    style: responsive all screens

--- src/components/core/header.tsx
+++ src/components/core/header.tsx
@@ -16,8 +16,10 @@ export function Header({ routeType, className, ...props }: Props) {
     <header
       data-protected-route={routeType === 'protected'}
       className={cn(
-        '-mb-1 flex h-56 w-full flex-col items-center justify-center gap-4 bg-celeste px-4 pt-1 text-zinc-50 lg:h-28 lg:flex-row lg:justify-between lg:px-20 lg:pt-0 xl:px-24 2xl:px-44',
-        routeType === 'protected' && 'h-64',
+        '-mb-1 flex h-56 w-full flex-col items-center justify-center gap-4 bg-celeste px-4 pt-1 text-zinc-50 xl:px-24 2xl:px-44',
+        routeType === 'protected'
+          ? 'h-64 lg:h-44 xl:h-28 xl:flex-row xl:justify-between xl:px-20 xl:pt-0'
+          : 'lg:h-28 lg:flex-row lg:justify-between lg:px-20 lg:pt-0',
         className,
       )}
       {...props}
