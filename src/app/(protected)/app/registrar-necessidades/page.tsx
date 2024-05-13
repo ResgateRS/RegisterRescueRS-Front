434ef60dc5f800befef937952188f1616180ceaa
Lorenzo <lorenzo.acetii@gmail.com>
2024-05-12 22:15:18 -0300

    style: responsive all screens

--- src/app/(protected)/app/registrar-necessidades/page.tsx
+++ src/app/(protected)/app/registrar-necessidades/page.tsx
@@ -1,3 +1,11 @@
+import { Section } from '@/app/(public)/_components/section'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 import { siteRoutes } from '@/config/site'
 import { cookiesNames } from '@/config/storage'
 import { jwtUserDataSchema } from '@/schemas/jwt-user-data-schema'
@@ -22,11 +30,21 @@ export default function RegistrarNecessidadesPage() {
   }
 
   return (
-    <div className="w-full">
-      <RegisterNeedsForm
-        shelterId={userData.data.shelterId}
-        authToken={token}
-      />
-    </div>
+    <Section className="mb-0 min-h-[calc(100vh-12.75rem)] items-center justify-center gap-7 pt-10 lg:min-h-[calc(100vh-6.75rem)] 2xl:gap-5">
+      <Card className="w-full lg:w-1/2">
+        <CardHeader>
+          <CardTitle>Registrar necessidades</CardTitle>
+          <CardDescription>
+            Preencha o formulário para salvar as necessidades do abrigo.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <RegisterNeedsForm
+            shelterId={userData.data.shelterId}
+            authToken={token}
+          />
+        </CardContent>
+      </Card>
+    </Section>
   )
 }
