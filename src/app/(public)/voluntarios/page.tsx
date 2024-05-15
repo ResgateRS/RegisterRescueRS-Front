929eaf00737e852a691e9ea9bcd5708b31501a05
Lorenzo Aceti <lorenzo.aceti@isdbflowtech.com>
2024-05-15 11:26:23 -0300

    refactor: change volunteer list to client component so it can pass coords

--- src/app/(public)/voluntarios/page.tsx
+++ src/app/(public)/voluntarios/page.tsx
@@ -1,14 +1,10 @@
 import { Section } from '@/app/(public)/_components/section'
-import { Suspense } from 'react'
-import { VolunteerListSkeleton } from './_components/volunteer-list-skeleton'
-import { VolunteerListWrapper } from './_components/volunteer-list-wrapper'
+import { VolunteerList } from './_components/volunteer-list'
 
 export default function VoluntariosPage() {
   return (
     <Section className="mb-0 min-h-[calc(100vh-12.75rem)] items-start justify-start gap-7 pt-10 lg:min-h-[calc(100vh-6.75rem)] 2xl:gap-5">
-      <Suspense fallback={<VolunteerListSkeleton />}>
-        <VolunteerListWrapper />
-      </Suspense>
+      <VolunteerList />
     </Section>
   )
 }
